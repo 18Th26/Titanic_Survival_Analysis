e26a7f50f4d5ad196d43835cc51522cc7a25332b
18Th26 <114772032+18Th26@users.noreply.github.com>
2024-12-14 00:08:04 +0800

    Update titanic_analysis.py
    add the name

--- PythonProject2/titanic_analysis.py
+++ PythonProject2/titanic_analysis.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 """
 Project: Titanic Survival Analysis
 Author: Shiping Yu
+        Tianhao Wang
 
 Description:
 This project analyzes the Titanic dataset to identify key factors influencing passenger survival rates.
